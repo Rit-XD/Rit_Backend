441d3dc91a2ad648d1916806e5ef10f0f2201da4
Bram-Colleman <r0820751@student.thomasmore.be>
2024-02-19 18:55:00 +0100

    feat: add models

---
+++ models/Volunteer.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const VolunteerSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  license: {
+    type: String,
+    required: true,
+  },
+  picture: {
+    type: String,
+    required: false,
+  },
+
+
+
+});
+
+const Volunteers = mongoose.model("Volunteers", VolunteerSchema);
+
+module.exports = Volunteers;
